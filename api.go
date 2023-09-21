a001a53b0cc0de150550d1c5526d89a13a52f34d
Tristan Goers <tristan.goers@fnsb.gov>
2023-09-21 11:55:13 -0800

    Update api.go

--- api.go
+++ api.go
@@ -14,10 +14,7 @@ func Get(config Config, path string, params url.Values) ([]byte, error) {
 	// 	url += fmt.Sprintf("?%s", params.Encode())
 	// }
 
-	//TODO: Header authroization
-
 	fmt.Println(url)
-
 
 	client := http.Client{}
 	req, err := http.NewRequest("GET", url, strings.NewReader(params.Encode()))
@@ -26,13 +23,12 @@ func Get(config Config, path string, params url.Values) ([]byte, error) {
 	if err != nil { fmt.Println("Request Error:", err) }
 	defer res.Body.Close()
 
-
-	
-	fmt.Println("A")
 	response, err := io.ReadAll(res.Body)
 	if err != nil { fmt.Println("io.ReadAll Error:", err) }
 
 	fmt.Println(string(response))
+	fmt.Println(req.RequestURI)
+	fmt.Println(strings.NewReader(params.Encode()))
 
 	return response, err
 }
