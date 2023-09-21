a0c33465557977e7282e94572cb9fd9c93602d52
Tristan Goers <tristan.goers@fnsb.gov>
2023-09-21 15:11:29 -0800

    Update tasks.go

--- tasks.go
+++ tasks.go
@@ -12,5 +12,5 @@ func QueryTasks_Get_Tasks(config Config, params url.Values) (types.Tasks, error)
 	response, _ := Get(config, path, params)
 
 	return types.TasksFromJSON(response)
-	// Comments5
+	// Comments6
 }
