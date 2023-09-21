d2c6b36b852e27fb58f20f3b21837b9b33b92a4c
Tristan Goers <tristan.goers@fnsb.gov>
2023-09-21 11:46:15 -0800

    Update api.go

--- api.go
+++ api.go
@@ -5,20 +5,29 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 func Get(config Config, path string, params url.Values) ([]byte, error) {
 	url := config.BaseUrl + path
-	if params != nil {
-		url += fmt.Sprintf("?%s", params.Encode())
-	}
+	// if params != nil {
+	// 	url += fmt.Sprintf("?%s", params.Encode())
+	// }
+
+	//TODO: Header authroization
 
 	fmt.Println(url)
 
-	res, err := http.Get(url)
+
+	client := http.Client{}
+	req, err := http.NewRequest("GET", url, strings.NewReader(params.Encode()))
+	req.Header.Add("Authorization", "Bearer " + config.PermAccessToken)
+	res, err := client.Do(req)
 	if err != nil { fmt.Println("Request Error:", err) }
 	defer res.Body.Close()
 
+
+	
 	fmt.Println("A")
 	response, err := io.ReadAll(res.Body)
 	if err != nil { fmt.Println("io.ReadAll Error:", err) }
